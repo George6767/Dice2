495c5ca7673610a3fc7046c152f51fa1844e1637
Kriz <kriz.dendropark@hotmail.com>
2025-01-03 19:02:48 +0300

    ok

--- Dice2/Program.cs
+++ Dice2/Program.cs
@@ -13,9 +13,15 @@ namespace Dice2
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press any key to roll the dice");
-            
+
+                Console.ReadKey();
+
                 playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a " + playerRandomNum);
             }
+
+            Console.ReadKey();
+
         }
     }
 }
